f93495901c29636ab8033b9dd4ec9c8035ed2c52
Vinicius-A-Siqueira <118227250+Vinicius-A-Siqueira@users.noreply.github.com>
2025-04-17 18:33:35 -0300

    Update HomeScreen.tsx

--- EcoConsciente/screens/HomeScreen.tsx
+++ EcoConsciente/screens/HomeScreen.tsx
@@ -19,7 +19,7 @@ export default function HomeScreen() {
       />
 
       <ImageCard
-        imageSource={require('../assets/natureza2.jpg')}
+        imageSource={require('../assets/natureza2.png')}
         caption="Cuidando do nosso planeta"
       />
     </View>
